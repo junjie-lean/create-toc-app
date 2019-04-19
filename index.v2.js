15e9cf2ed5fb90f742e2ab9fce71a340d93c19e3
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-04-19 22:18:18 +0800

    可执行验证

--- index.v2.js
+++ index.v2.js
@@ -4,7 +4,7 @@
  * @Author: junjie.lean
  * @Date: 2019-04-19 10:16:17
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2019-04-19 17:51:04
+ * @Last Modified time: 2019-04-19 22:09:35
  */
 
 /**
@@ -28,27 +28,45 @@ const GIT_OPTION = {
   }
 };
 
-let isDownLoad = false;
-
 /**
  * @description 主程
  */
 program.version(require("./package.json").version, "-v,--version");
-program.option("-s --serverless", "ues toc have server").action(dir => {
-  //判断dirname是否合法
-  if (!isAllowCreate()) {
-    throw new Error("not all");
-  }
+program
+  .option("-s --serverless", "ues toc have NOT server")
+  .option(
+    "-f --force",
+    "force to create project(it will delete if have a same dir!)"
+  )
+  .action(dir => {
+    //判断dirname是否合法
+    if (!isAllowCreate(dir)) {
+      // throw new Error("not all");
+      console.log("❌  '<" + dir + ">'是一个不合适的项目名称");
+      return false;
+    }
 
-  //判断当前文件夹是否有同名文件夹
-  if (checkRepeat(dir)) {
-    createFloder(dir);
-    getTemplate(dir, program.serverless);
-  } else {
-    console.warn("❌   当前目录下已有<" + dir + ">文件夹");
-    return false;
-  }
-});
+    if (program.force) {
+      if (!checkRepeat(dir)) {
+        console.log("⚠️  即将删除项目<" + dir + ">");
+        deleteFolder(path.join(process.cwd(), dir));
+      }
+
+      createFloder(dir);
+      getTemplate(dir, program.serverless);
+
+      return false;
+    }
+
+    //判断当前文件夹是否有同名文件夹
+    if (checkRepeat(dir)) {
+      createFloder(dir);
+      getTemplate(dir, program.serverless);
+    } else {
+      console.warn("❌   当前目录下已有<" + dir + ">文件夹");
+      return false;
+    }
+  });
 
 program.parse(process.argv);
 
@@ -57,6 +75,11 @@ program.parse(process.argv);
  * @param {String} dirname
  */
 function isAllowCreate(dirname) {
+  let reg = /(^\w{1,64}$)/gi;
+  if (!reg.test(dirname)) {
+    return false;
+  }
+
   return true;
 }
 
@@ -65,7 +88,7 @@ function isAllowCreate(dirname) {
  * @param {String} dirname
  */
 function createFloder(dirname) {
-  console.log("✔️  开始创建项目结构<" + dirname + ">");
+  console.log("✔️  开始创建项目“<" + dirname + ">”,请稍等...");
   fs.mkdirSync(dirname);
 }
 
@@ -98,31 +121,60 @@ function checkRepeat(dirname) {
  */
 function getTemplate(dirname, serverless) {
   if (serverless) {
-    console.log("✔️  开始初始化toc.serverless目录结构");
-    download(
-      `${GIT_OPTION.serverless.addr}#${GIT_OPTION.serverless.branch}`,
-      path.join(process.cwd(), dirname),
-      err => {
-        if (err || isDownLoad) {
-          console.log("❌  无法拉取模板");
-        } else {
-          insDepend(dirname);
-        }
-      }
+    console.log(
+      "✔️  开始初始化toc.serverless目录结构(这可能需要花费一点时间)..."
     );
+
+    new Promise((resolve, reject) => {
+      download(
+        `${GIT_OPTION.serverless.addr}#${GIT_OPTION.serverless.branch}`,
+        path.join(process.cwd(), dirname),
+        err => {
+          if (err) {
+            console.log("❌  无法拉取模板");
+            reject();
+          } else {
+            resolve();
+          }
+        }
+      );
+    })
+      .then(() => {
+        //返回异步子进程对象,方便后续处理
+        return insDepend(dirname);
+      })
+      .then(cp => {
+        //insdep Child_process
+      })
+      .catch(err => {
+        console.log(err);
+      });
   } else {
-    console.log("✔️  开始初始化toc目录结构");
-    download(
-      `${GIT_OPTION.server.addr}#${GIT_OPTION.server.branch}`,
-      path.join(process.cwd(), dirname),
-      err => {
-        if (err || isDownLoad) {
-          console.log("❌  无法拉取模板");
-        } else {
-          insDepend(dirname);
+    console.log("✔️  开始初始化toc目录结构(这可能需要花费一点时间)...");
+    new Promise((resolve, reject) => {
+      download(
+        `${GIT_OPTION.server.addr}#${GIT_OPTION.server.branch}`,
+        path.join(process.cwd(), dirname),
+        err => {
+          if (err) {
+            console.log("❌  无法拉取模板");
+            reject();
+          } else {
+            // insDepend(dirname);
+            resolve();
+          }
         }
-      }
-    );
+      );
+    })
+      .then(() => {
+        return insDepend(dirname);
+      })
+      .then(cp => {
+        //insdep Child_process
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
@@ -130,23 +182,14 @@ function getTemplate(dirname, serverless) {
  * @description 安装依赖的
  */
 function insDepend(dirname) {
-  console.log("✔️  项目目录初始化完毕，开始安装依赖");
-  //   let insDep = spawn.sync("npm", ["i"], {
-  //     cwd: path.join(process.cwd(), dirname),
-  //     encoding: "utf8",
-  //     shell: process.platform == "win32",
-  //     // stdio: ["inherit", "ignore", "pipe"]
-  //     stdio: "inherit"
-  //   });
-  let insDep = spawn.sync("echo", ["nihao "], {
+  console.log("✔️  项目目录初始化完毕，开始安装依赖...");
+  let insDep = spawn("npm", ["i"], {
     cwd: path.join(process.cwd(), dirname),
     encoding: "utf8",
     shell: process.platform == "win32",
-    // stdio: ["inherit", "ignore", "pipe"]
-    stdio: "inherit"
+    stdio: ["inherit", "ignore", "pipe"]
+    //   stdio: "inherit"
   });
-
-  isDownLoad = true;
   insDep.on("close", (code, signal) => {
     if (code == 0 && !signal) {
       console.log("✔️  依赖安装完毕");
@@ -154,4 +197,32 @@ function insDepend(dirname) {
       console.log("❌   依赖安装失败");
     }
   });
+  return insDep;
+}
+
+/**
+ *
+ * @description 删除指定文件或文件夹
+ */
+function deleteFolder(path) {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(file => {
+      let curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        deleteFolder(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
+
+/**
+ * @description print项目初始化介绍
+ */
+function showIntro() {
+  console.log();
 }
